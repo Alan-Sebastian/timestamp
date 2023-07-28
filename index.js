36a50b06f1bc0a2703aa3817799362cc565b5d89
Alan-Sebastian <alansebastian740@gmail.com>
2023-07-28 13:53:13 +0530

    first commit

--- index.js
+++ index.js
@@ -21,12 +21,38 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
+const isInvalid =(date) => date.toUTCString() ==="invalid date"
+app.get("/api/:date", function (req, res) {
+  let date= new Date(req.params.date)
+  if(isInvalid(date)){
+   date=new Date(+req.params.date)
+  }
+  if(isInvalid(date))
+  {
+    res.json({
+      error:"invalid date"
+    });
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+app.get("/api",(req, res) =>{
+  res.json({
+    unix: new Date().getTime(),
+     utx: new Date().toUTCString()
+  })
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(4000, function () {
+  console.log('Your app is listening on port ' + 4000);
 });
